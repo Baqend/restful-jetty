755b9acfa96a89bb33571e1372aaff7232c42985
fbuecklers <fb@19f.de>
2013-05-02 16:57:01 +0200

    add jetty lib definition

--- src/info/orestes/rest/client/RestClient.java
+++ src/info/orestes/rest/client/RestClient.java
@@ -4,11 +4,9 @@ import info.orestes.rest.conversion.ConverterService;
 import info.orestes.rest.conversion.MediaType;
 
 import java.net.URI;
-import java.util.List;
 
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.Request;
-import org.eclipse.jetty.client.api.Response.ResponseListener;
 
 public class RestClient extends HttpClient {
 	
@@ -25,12 +23,6 @@ public class RestClient extends HttpClient {
 	@Override
 	public Request newRequest(String path) {
 		return new RestRequest(this, baseURI, path);
-	}
-	
-	@Override
-	protected void send(Request request, List<ResponseListener> listeners) {
-		
-		super.send(request, listeners);
 	}
 	
 	public ConverterService getConverterService() {
