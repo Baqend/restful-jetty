678fc05776ca007beb8c33045730e0153a769f54
fbuecklers <fbuecklers@gmail.com>
2013-06-23 14:46:50 +0200

    cleanup

--- main/src/info/orestes/rest/util/ResourceSet.java
+++ main/src/info/orestes/rest/util/ResourceSet.java
@@ -1,20 +1,22 @@
 package info.orestes.rest.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.TreeSet;
 
 public class ResourceSet extends TreeSet<String> {
 	
+	private static final long serialVersionUID = 1L;
 	public static final Comparator<String> RESOURCE_COMPARATOR = new Comparator<String>() {
 		@Override
 		public int compare(String o1, String o2) {
 			if (!o1.endsWith("/")) {
-				if (o2.endsWith("/"))
+				if (o2.endsWith("/")) {
 					return -1;
-			} else if (!o2.endsWith("/"))
+				}
+			} else if (!o2.endsWith("/")) {
 				return 1;
+			}
 			
 			return o1.compareToIgnoreCase(o2);
 		}
@@ -23,7 +25,7 @@ public class ResourceSet extends TreeSet<String> {
 	public ResourceSet() {
 		super(RESOURCE_COMPARATOR);
 	}
-
+	
 	public ResourceSet(Collection<String> classes) {
 		this();
 		
