c394ded0f0023da15d3d59bf076d78d41e980d96
fbuecklers <fbuecklers@gmail.com>
2014-03-05 17:50:34 +0100

    change project structure to intellij projects

--- main/src/info/orestes/rest/service/ServiceDocumentTypes.java
+++
@@ -1,9 +0,0 @@
-package info.orestes.rest.service;
-
-public interface ServiceDocumentTypes {
-	
-	public Class<?> getEntityClassForName(String name);
-	
-	public Class<?> getArgumentClassForName(String name);
-	
-}
--- main/src/info/orestes/rest/service/Spec.java
+++
@@ -1,7 +0,0 @@
-package info.orestes.rest.service;
-
-import java.util.LinkedList;
-
-public class Spec extends LinkedList<MethodGroup> {
-	
-}
